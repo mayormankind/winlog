7108d771a6679a08165c138d7e414b10cb2c7410
mayowa <mayowamakinde23@gmail.com>
2025-07-22 14:43:04 +0100

    supabaseClient error handling

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,10 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Missing Supabase environment variables");
-}
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
